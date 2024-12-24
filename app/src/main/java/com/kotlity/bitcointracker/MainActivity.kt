30bdc86798b757e1fbbb6e19b95e350ff8c06868
Kotlity <frankierafie12@gmail.com>
2024-12-24 15:51:02 +0200

    Initial commit

---
+++ app/src/main/java/com/kotlity/bitcointracker/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.kotlity.bitcointracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.kotlity.bitcointracker.ui.theme.BitcoinTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BitcoinTrackerTheme {
+
+            }
+        }
+    }
+}
