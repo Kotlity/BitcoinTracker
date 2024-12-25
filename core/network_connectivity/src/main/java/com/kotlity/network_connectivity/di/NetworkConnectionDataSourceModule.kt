5482054caaa34ab9a33b4b1cff4ac3bdaabb11e7
Kotlity <frankierafie12@gmail.com>
2024-12-25 23:11:11 +0200

    Added NetworkConnectionViewModel

--- core/network_connectivity/src/main/java/com/kotlity/network_connectivity/di/NetworkConnectionDataSourceModule.kt
+++ core/network_connectivity/src/main/java/com/kotlity/network_connectivity/di/NetworkConnectionDataSourceModule.kt
@@ -1,5 +1,7 @@
-package com.kotlity.network_connectivity
+package com.kotlity.network_connectivity.di
 
+import com.kotlity.network_connectivity.DefaultNetworkConnectionDataSource
+import com.kotlity.network_connectivity.NetworkConnectionDataSource
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
