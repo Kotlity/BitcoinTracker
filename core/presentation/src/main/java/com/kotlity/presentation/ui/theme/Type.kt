dec862d71e54c1d7f1a04fcac9e92accd3ccfb17
Kotlity <frankierafie12@gmail.com>
2024-12-25 22:43:57 +0200

    Added core/presentation module

--- core/presentation/src/main/java/com/kotlity/presentation/ui/theme/Type.kt
+++ core/presentation/src/main/java/com/kotlity/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.kotlity.bitcointracker.ui.theme
+package com.kotlity.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,4 @@ dependencyResolutionManagement {
 rootProject.name = "BitcoinTracker"
 include(":app")
 include(":core:domain")
+include(":core:presentation")
